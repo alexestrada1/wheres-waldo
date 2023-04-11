7c3dd1840d34d267d8d141b2d95f81e6473f5284
alexestrada1 <estradaalex46@gmail.com>
2023-04-10 19:23:52 -0500

    Got the data.time to only store when the current time is greater that the stored store

--- src/App.js
+++ src/App.js
@@ -2,45 +2,55 @@ import React, { useRef, useState } from "react";
 import Game from "./Components/Game/Game";
 import Header from "./Components/Header/Header";
 import { firestore } from "./firebase";
-import { setDoc, doc, collection, updateDoc } from "firebase/firestore";
+import { setDoc, doc, collection, updateDoc, getDoc } from "firebase/firestore";
 
 function App() {
   const [gameStarted, setGameStarted] = useState(false);
   const [playerName, setPlayerName] = useState("");
-  const [pause, setPause] = useState(false)
-  const [score, setScore] = useState(0)
+  const [pause, setPause] = useState(false);
+  const [score, setScore] = useState(0);
   const playerNameRef = useRef();
 
   function stopTimer() {
-    setPause(true)
-  }
-  function getScore(x){
-    setScore(x)
+    setPause(true);
   }
 
-  function handleRestartGame() {
+  function getScore(x) {
+    setScore(x);
+  }
+
+  async function handleRestartGame() {
     const docRef = doc(collection(firestore, "names"), playerName);
-    const data = {
-      time: score,
-    };
+
     try {
-      updateDoc(docRef, data);
+      const docSnap = await getDoc(docRef);
+      const existingData = docSnap.data();
+      const existingTime = existingData?.time || 0;
+      if (score < existingTime || existingTime === 0) {
+        const data = {
+          time: score,
+        };
+        await updateDoc(docRef, data);
+      }
     } catch (e) {
       console.log(e);
     }
+
     setGameStarted(false);
   }
 
   const handleStartGame = async () => {
-    setPause(false)
+    setPause(false);
     const name = playerNameRef.current.value;
     setPlayerName(name);
     const docRef = doc(collection(firestore, "names"), name);
-    const data = {
-      time: "",
-    };
+
     try {
-      await setDoc(docRef, data);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        // if the document doesn't exist yet, create it with an empty time field
+        await setDoc(docRef, { time: "" });
+      }
     } catch (e) {
       console.log(e);
     }
@@ -49,7 +59,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header gameStarted={gameStarted} pause={pause} getScore={getScore}/>
+      <Header gameStarted={gameStarted} pause={pause} getScore={getScore} />
       {!gameStarted && (
         <div className="start-game">
           <label>Enter Name</label>
